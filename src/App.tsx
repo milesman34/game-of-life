b0da82190e1c23e7449e055a684e31b3623cc6b8
milesman34 <milesmanmusic@yahoo.com>
2024-07-09 12:59:18 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./common.css";
+import "./index.css";
+
+const App = () => {
+    return (
+        <div className="app">
+            <div className="flex-center header-text">
+                Game of Life
+            </div>
+        </div>
+    );
+}
+
+export default App;
